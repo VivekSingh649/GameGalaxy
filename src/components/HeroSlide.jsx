365de62251f7f11acd8d58d1b7d2227338b1f0a4
Vivek Singh <rajputvivek649@gmail.com>
2024-04-23 22:33:43 +0530

    lik ad

--- src/components/HeroSlide.jsx
+++ src/components/HeroSlide.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeroSlide(props) {
     return (
-        <section className="text-gray-600 body-font relative z-0"
+        <section className="text-gray-600 body-font relative z-0 py-5 lg:py-16"
             style={{
                 backgroundImage: `url("${props.backgroundImage}")`,
                 backgroundSize: 'cover',
@@ -10,12 +10,12 @@ function HeroSlide(props) {
                 backgroundPosition: 'center',
             }}>
             <div className="overlay"></div>
-            <div className="max-w-screen-xl mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center hero_content">
-                    <h1 className="title-font sm:text-4xl md:text-6xl mb-4 text-white">{props.heroTitle}
+            <div className="max-w-screen-xl mx-auto flex px-5 md:flex-row flex-col items-center py-24">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    <h1 className="text-5xl md:text-6xl mb-4 text-white font-semibold">{props.heroTitle}
                         <br className="hidden lg:inline-block" />
                     </h1>
-                    <p className="mb-8 leading-relaxed text-xl font-semibold text-white">{props.heroSubTitle}</p>
+                    <p className="mb-8 leading-relaxed text-white  font-semibold">{props.heroSubTitle}</p>
                     <div className="flex justify-center">
                         <a href="https://damangames.in/#/register?invitationCode=485767061548" type="button" className="px-6 py-3.5 text-base font-medium text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             <i className="bi bi-controller mr-2"></i>
--- src/components/MostPopular.jsx
+++ src/components/MostPopular.jsx
@@ -9,7 +9,7 @@ function MostPopular() {
                     <h1 className="text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center mb-20"><span className="text-transparent bg-clip-text bg-gradient-to-r to-blue-600 from-sky-400">Trending</span> Games</h1>
                     <div className="grid_container">
                         <div className="space-y-8">
-                            <div className="relative rounded-md">
+                            <div className="relative rounded-md h-[300px] md:h-auto">
                                 <img
                                     src={pictures.OneShotFishing}
                                     alt="AirMax Pro"
@@ -24,7 +24,7 @@ function MostPopular() {
                                     <a href="https://damangames.in/#/" className="mt-3 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">Play Now<svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"></path></svg></a>
                                 </div>
                             </div>
-                            <div className="relative rounded-md">
+                            <div className="relative rounded-md h-[300px] md:h-auto">
                                 <img
                                     src={pictures.CarromPool}
                                     alt="AirMax Pro"
@@ -41,7 +41,7 @@ function MostPopular() {
                             </div>
                         </div>
                         <div className="space-y-8">
-                            <div className="relative rounded-md">
+                            <div className="relative rounded-md h-[300px] md:h-auto">
                                 <img
                                     src={pictures.cricektLegue}
                                     alt="AirMax Pro"
@@ -56,7 +56,7 @@ function MostPopular() {
                                     <a href="https://damangames.in/#/" className="mt-3 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">Play Now<svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"></path></svg></a>
                                 </div>
                             </div>
-                            <div className="relative rounded-md">
+                            <div className="relative rounded-md h-[300px] md:h-auto">
                                 <img
                                     src={pictures.moneyComming}
                                     alt="AirMax Pro"
@@ -71,7 +71,7 @@ function MostPopular() {
                                     <a href="https://damangames.in/#/" className="mt-3 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">Play Now<svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"></path></svg></a>
                                 </div>
                             </div>
-                            <div className="relative rounded-md">
+                            <div className="relative rounded-md h-[300px]">
                                 <img
                                     src={pictures.MountainClimb}
                                     alt="AirMax Pro"
